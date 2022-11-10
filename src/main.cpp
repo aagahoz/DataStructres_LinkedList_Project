03185e4fd39d2425b207b5a015a6c36ad6b5b7ff
aagahoz <ozdemiragah@gmail.com>
2022-11-11 00:54:11 +0300

    Manage and Row Class Methods updated and created

--- src/main.cpp
+++ src/main.cpp
@@ -14,25 +14,57 @@ int main()
     int newArray[5] = {1, 2, 3, 4, 5};
 
     RowListNode *headRow1 = new RowListNode();
+
+
+    headRow1->push_back(42);
+    headRow1->push_back(23);
+    headRow1->push_back(33);
+    headRow1->push_back(15);
+
+    headRow1->printList();
+
+    headRow1->deleteNode(33);
+    headRow1->printList();
+
+    cout << "Found at index " << headRow1->findNodeIndex(15) << endl;
+
     RowListNode *headRow2 = new RowListNode();
+    headRow2->push_back(5);
+    headRow2->push_back(6);
+
     RowListNode *headRow3 = new RowListNode();
-
-    headRow1->push_back(1);
-    headRow1->push_back(2);
-
-    headRow2->push_back(3);
-    headRow2->push_back(4);
-    headRow3->push_back(5);
-    headRow3->push_back(6);
+    headRow3->push_back(7);
+    headRow3->push_back(8);
 
     ManagerListNode *headManager = new ManagerListNode();
 
-    headManager->push_back(headRow1);
+
+    cout << endl << endl << endl;
     headManager->push_back(headRow2);
+    headManager->push_back(headRow1);
     headManager->push_back(headRow3);
     headManager->printList();
 
-    headManager->cleanList();
+    headManager->sortManageListByRowListAverage();
+    
+    cout << endl << endl << endl;
+    
+    headManager->printList();
+
+    // headManager->cleanList();
+
+    RowListNode *headRow4 = new RowListNode();
+
+    headRow4->push_back(1);
+    headRow4->push_back(1);
+    headRow4->push_back(1);
+    headRow4->push_back(1);
+    headManager->push_back(headRow4);
+    cout << endl << endl << endl;
+
+    headManager->printList();
+
+    
 
     system("pause");
     return 0;
